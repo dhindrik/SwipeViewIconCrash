b33966f0da5a0e8fd607ba4857ac61ab3007137c
Daniel Hindrikes <daniel@hindrikes.se>
2019-12-06 13:21:04 +0100

    initial commit

---
+++ SwipeViewIconCrash/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace SwipeViewIconCrash
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<string> Cars { get; set; } = new ObservableCollection<string>(new List<string>()
+        {
+            "Audi",
+            "BMW",
+            "Mercedes",
+            "Volvo",
+            "Opel",
+            "Skoda",
+            "Volkswagen"
+        });
+ 
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
